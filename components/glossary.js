8e06686eadbbf7d2bddeba2d2b5c59d1fe575638
Thomas Wu <62056970+thomaswcy@users.noreply.github.com>
2023-01-23 00:49:46 +0800

    Update glossary.js

--- components/glossary.js
+++ components/glossary.js
@@ -41,19 +41,19 @@ export default function Glossary() {
             columns={[1,2]}
           >
             <Box>
-              <b>Venue:</b> Masters' Union <br />
-              Tower A, DLF Cyberpark, <br />
-              Phase II, Udyog Vihar, Sector 20,
+              <b>Venue:</b> Placeholder <br />
+              Placeholder, Placeholder, <br />
+              Placeholder, Placeholder, Placeholder,
               <br />
-              Gurugram, Delhi NCR
+              Placeholder, Placeholder
               <Grid columns={2} mt={2} sx={{ width: "80%", display: ['none!important', 'grid!important'] }}>
                 <Image
-                  alt="Image of Gurugram's skyline in the Delhi NCR"
+                  alt="Placeholder"
                   sx={{ borderRadius: 4, objectFit: 'cover', height: '200px' }}
                   src="https://upload.wikimedia.org/wikipedia/commons/c/c2/Cyber_City_Skyline.jpeg"
                 />
                 <Image
-                  alt="Image of Gurugram's metro in the Delhi NCR"
+                  alt="Placeholder"
                   sx={{ borderRadius: 4, objectFit: 'cover', height: '200px' }}
                   src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Cyber_City_View.jpg/2560px-Cyber_City_View.jpg"
                 />
@@ -84,31 +84,6 @@ export default function Glossary() {
             }}
             columns="1fr 1fr"
           >
-            <Box mb={2}>
-              <b>₹12.5 Lakh in Travel Stipends: </b>Epoch is for hackers across
-              India and the world; with that in mind, we're providing ₹12.5 lakh
-              in travel stipends to both domestic and international attendees.
-
-            </Box>
-            <Box>
-              We'll cover the full cost of a return flight to New Delhi for
-              stipend recipients in India and provide a USD 500 stipend to
-              recipients outside of India. Travel stipends can be requested
-              on the registration form below.
-            </Box>
-          </Box>
-          <Box
-            sx={{
-              bg: "white",
-              color: "slate",
-              p: [3,4],
-              borderRadius: 4,
-              border: "0.7px solid",
-              borderColor: "slate",
-              fontSize: ["18px",2],
-            }}
-            columns="1fr 1fr"
-          >
             <Box>
               <Box mb={2}>
                 <b>Hack Club</b> is a worldwide community of high school
@@ -118,15 +93,15 @@ export default function Glossary() {
               </Box>
               <Box>
                 Epoch is fully open source, funded by, and  run by the Hack Club community.
-                Hack Clubbers are organising Epoch in public on the{" "}
+                Hack Clubbers are organising Reboot in public on the{" "}
                   <Link href="https://hackclub.com/slack" target="_blank">
                   Hack Club Slack
                 </Link>
                 {" "}and with fully transparent finances using{" "}
-                <Link href="https://bank.hackclub.com/epoch" target="_blank">
+                <Link href="https://bank.hackclub.com/reboot" target="_blank">
                   Hack Club Bank
                 </Link>.
-                Everything from this website to Epoch's dinner menu, was
+                Everything from this website to Reboot's dinner menu, was
                 created by teenagers just like yourself and you're welcome to
                 help out in <i>#epoch-bts</i> on{" "}
                 <Link href="https://hackclub.com/slack" target="_blank">
@@ -140,7 +115,7 @@ export default function Glossary() {
         <Grid columns={[1, 2]}>
           <Grid sx={{ height: "fit-content" }}>
             <GlossaryItem
-              question={`Am I eligible to participate in Epoch?`}
+              question={`Am I eligible to participate in Reboot?`}
               answer={
                 <>
                   If you're eighteen or under, yes, we are so excited to see you
@@ -148,16 +123,16 @@ export default function Glossary() {
                   shoot us a mail at{" "}
                   <a
                     style={{ color: "black" }}
-                    href="mailto:epoch@hackclub.com"
+                    href="mailto:thomaswcy@linkscape.app"
                   >
-                    epoch@hackclub.com
+                    thomaswcy@linkscape.app
                   </a>
                   .
                 </>
               }
             />
             <GlossaryItem
-              question={`What do I need to bring to Epoch?`}
+              question={`What do I need to bring to Reboot?`}
               answer={`Your laptop, chargers, anything for your hack (hardware?),
           toiletries, sleeping bags, and an open mind.`}
             />
@@ -194,12 +169,7 @@ export default function Glossary() {
                   </a>{" "}
                   and we'll make sure to answer all their questions. Epoch will
                   be supervised by background checked staff and overseen by 24/7
-                  security staff. Read more in our {" "}
-                  <a
-                    style={{ color: "black" }}
-                    href="https://hack.af/epoch-parents"
-                  >
-                    parent's guide
+                  security staff. 
                   </a>.
                 </>
               }
@@ -212,7 +182,7 @@ export default function Glossary() {
             hackathon, as well as swag, prizes, and fun mini-events.`}
             />
             <GlossaryItem
-              question="What can I make at Epoch?"
+              question="What can I make at Reboot?"
               answer={
                 <>
                   Anything! Apps, art, sites, and hardware projects are all
@@ -252,20 +222,12 @@ export default function Glossary() {
               question="I have more questions, how can I reach out?"
               answer={
                 <>
-                  Contact us! Reach out on #epoch on the{" "}
+                  Contact us! Email us at {" "}
                   <a
                     style={{ color: "black" }}
-                    href="https://hackclub.com/slack"
-                    target="_blank"
+                    href="mailto:thomaswcy@linkscape.app"
                   >
-                    Hack Club Slack
-                  </a>
-                  {" "}or email us at {" "}
-                  <a
-                    style={{ color: "black" }}
-                    href="mailto:epoch@hackclub.com"
-                  >
-                    epoch@hackclub.com
+                    thomaswcy@linkscape.app
                   </a>{" "}
                   We're always ready to answer all your questions!
                 </>
